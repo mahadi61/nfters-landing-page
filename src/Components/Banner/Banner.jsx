7c2946cbedaca594dca294fd63140960155c8ba0
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-12-21 02:04:16 +0600

    banner text done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className="flex justify-center items-center min-h-screen">
       <div className="w-[70%] md:w-11/12 lg:max-w-7xl flex flex-col md:flex-row justify-between items-center">
         <div className="md:w-1/2 flex flex-col ">
-          <h1 className="text-3xl md:text-5xl text-black font-bold mb-4">
+          <h1 className="text-3xl md:text-5xl text-black font-bold mb-4 uppercase">
             Discover, and collect Digital Art NFTs{" "}
           </h1>
           <p className="text-[#565656] text-xl mb-10">
