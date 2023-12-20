2bac9abe35b27a9d71de0d85ca5c8ce1a58e36d5
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-12-20 23:03:28 +0600

    data loaded done

--- src/Components/Discover/DiscoverCard.jsx
+++ src/Components/Discover/DiscoverCard.jsx
@@ -1,28 +1,20 @@
 import Eth from "../../utilities/Eth";
 
 const DiscoverCard = ({ data }) => {
-  console.log(data);
   const { image, name, ETH, time_left, total_items_left, people } = data;
   return (
     <div className="bg-white p-3 rounded-xl ">
       <div className="relative">
         <img src={image} alt="" className="h-[222px] w-[247px] object-cover" />
         <div className="flex items-center absolute left-5 -bottom-3">
-          <img
-            src="../../../public/image/collection/author.png"
-            alt=""
-            className="border-2 border-[#ffff] rounded-[30px] w-[30px] h-[30px] -ms-2"
-          />
-          <img
-            src="../../../public/image/collection/author.png"
-            alt=""
-            className="border-2 border-[#ffff] rounded-[30px] w-[30px] h-[30px] -ms-2"
-          />
-          <img
-            src="../../../public/image/collection/author.png"
-            alt=""
-            className="border-2 border-[#ffff] rounded-[30px] w-[30px] h-[30px] -ms-2"
-          />
+          {people.map((man, i) => (
+            <img
+              key={i}
+              src={man?.image}
+              alt=""
+              className="border-2 border-[#ffff] rounded-[30px] w-[30px] h-[30px] -ms-2"
+            />
+          ))}
         </div>
       </div>
       <div>
@@ -33,11 +25,11 @@ const DiscoverCard = ({ data }) => {
             color={"text-[#00AC4F]"}
             fill={"#00AC4F"}
           ></Eth>
-          <p className="text-[#838383]">1 of 321</p>
+          <p className="text-[#838383]">{total_items_left}</p>
         </div>
         <div className="flex items-center justify-between font-semibold text-[#4F33A3]">
           <p className="bg-[#DCDCDC33] px-[10px] py-1 rounded-[109px]">
-            3h 50m 2s left
+            {`${time_left} left`}
           </p>
           <button>Place a bid</button>
         </div>
