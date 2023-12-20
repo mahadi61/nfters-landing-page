2d5d19735851b9d6026744ca07471a2cd59ab04e
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-12-21 01:07:04 +0600

    collection sectiono resposive done

--- src/Components/Collection/Collection.jsx
+++ src/Components/Collection/Collection.jsx
@@ -11,12 +11,12 @@ const Collection = () => {
   }, []);
 
   return (
-    <div className="bg-[#D9E0EC33] h-[619px] flex items-center justify-center">
+    <div className="bg-[#D9E0EC33] py-10  md:h-[619px] flex items-center justify-center">
       <div className="w-11/12 lg:max-w-7xl mx-auto">
-        <h1 className="text-black text-3xl font-bold uppercase mb-14">
+        <h1 className="text-black text-2xl md:text-3xl font-bold uppercase mb-14">
           Collection Featured NFTs
         </h1>
-        <div className="flex justify-between gap-8">
+        <div className="flex flex-col md:flex-row justify-between gap-8">
           {collection.map((nftData, i) => (
             <CollectionCard key={i} nftData={nftData}></CollectionCard>
           ))}
